1743e57a79849782b780267c7aedc7553212d843
satheesh gollapudi <gskumar3107@gmail.com>
2022-10-30 11:12:26 +1300

    Commit

--- MarsFramework/Test/ShareSkillTest.cs
+++ MarsFramework/Test/ShareSkillTest.cs
@@ -16,30 +16,7 @@ namespace MarsFramework.Test
         class User : Global.Base
         {
 
-           /* [Test]
-            public void addSkill()
-            {
-
-                try
-                {
-                    // Creates a toggle for the given test, adds all log events under it    
-                    test = extent.StartTest(" Profile");
-
-                    //GlobalDefinitions globalDefinitions = new GlobalDefinitions();
-
-                    // Create an class and object to call the method
-                    ShareSkill shareSkill = new ShareSkill();
-                    shareSkill.EnterShareSkill();
-                    test.Log(LogStatus.Pass, "Test Passed");
-                    Console.WriteLine("Test Passed");
-                }
-                catch (Exception e)
-                {
-                    test.Log(LogStatus.Fail, "Test Failed", e.Message);
-                    Console.WriteLine("Test Failed");
-                }
-
-            }*/
+          
 
 
             [Test]
